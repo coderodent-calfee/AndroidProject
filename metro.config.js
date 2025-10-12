6603a78dfb178f0d9367d4fa14dd33e366231b8e
coderodent-calfee <robert.w.calfee@gmail.com>
2025-10-11 17:34:29 -0700

    buttons text png svg all ok for android

--- metro.config.js
+++ metro.config.js
@@ -6,6 +6,18 @@ const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = {};
+module.exports = (async () => {
+  const defaultConfig = await getDefaultConfig(__dirname);
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+  const { assetExts, sourceExts } = defaultConfig.resolver;
+
+  return mergeConfig(defaultConfig, {
+    transformer: {
+      babelTransformerPath: require.resolve('react-native-svg-transformer'),
+    },
+    resolver: {
+      assetExts: assetExts.filter(ext => ext !== 'svg'),
+      sourceExts: [...sourceExts, 'svg'],
+    },
+  });
+})();
